9321990d81bd7bbeb3ef0d8a44db5acef5526dc3
Valeriy Stepanov <stepanovv.ru@yandex.ru>
2019-08-14 19:34:32 +0300

    testing

---
+++ src/testing.ts
@@ -0,0 +1,11 @@
+import { IRunListItem } from "./utils";
+
+
+/**
+ * Тестирование операторов и потоков
+ * 
+ * TODO заполнить примерами  https://medium.com/@kevinkreuzer/marble-testing-with-rxjs-testing-utils-3ae36ac3346a
+ */
+
+export const testingOperatorList: IRunListItem[] = [];
+
